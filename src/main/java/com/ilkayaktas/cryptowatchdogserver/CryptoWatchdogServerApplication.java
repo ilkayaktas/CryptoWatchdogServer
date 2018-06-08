b49e2924315ef4df8bf7459143cfcf01a3305195
İlkay Aktaş <ilkayaktas@gmail.com>
2018-06-08 16:05:44 +0300

    Project is initialized.

---
+++ src/main/java/com/ilkayaktas/cryptowatchdogserver/CryptoWatchdogServerApplication.java
@@ -0,0 +1,12 @@
+package com.ilkayaktas.cryptowatchdogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWatchdogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoWatchdogServerApplication.class, args);
+    }
+}
